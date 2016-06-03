f46eb081e1356223858ef91d115942363592ea32
DennisDaddy <softteez95@gmail.com>
2016-06-03 19:54:25 +0300

    finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,32 +2,26 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-@user = User.new(name: "Example User", email: "user@example.com")
-   password: "foobar", password_confirmation: "foobar")
+@user = User.new(name: "Example User", email: "user@example.com" ,password: "foobar", password_confirmation: "foobar")
 end
 
 test "password should have a minimum length" do
-@user.password = @user.password_confirmation = "a" * 5
-assert_not @user.valid?
+ @user.password = @user.password_confirmation = "a" * 5
+ assert_not @user.valid?
 end
-
-test "email addresses should be unique" do
-duplicate_user = @user.dup
-duplicate_user.email = @user.email.upcase
-@user.save
-assert_not duplicate_user.valid?
 
 test "should be valid" do
 assert @user.valid?
 end
 
+
 test "name should be present" do
 @user.name = ""
 assert_not @user.valid?
 end
+
 test "email should be present" do
-@user.email = "
-"
+@user.email = " "
 assert_not @user.valid?
 end
 
@@ -35,16 +29,10 @@ test "name should not be too long" do
 @user.name = "a" * 51
 assert_not @user.valid?
 end
+
 test "email should not be too long" do
 @user.email = "a" * 244 + "@example.com"
 assert_not @user.valid?
-end
-
-test "email addresses should be unique" do
-duplicate_user = @user.dup
-duplicate_user.email = @user.email.upcase
-@user.save
-assert_not duplicate_user.valid?
 end
 
 test "email validation should accept valid addresses" do
@@ -53,6 +41,13 @@ first.last@foo.jp alice+bob@baz.cn]
 valid_addresses.each do |valid_address|
 @user.email = valid_address
 assert @user.valid?, "#{valid_address.inspect} should be valid"
+  end
 end
+
+test "email addresses should be unique" do
+duplicate_user = @user.dup
+duplicate_user.email = @user.email.upcase
+@user.save
+assert_not duplicate_user.valid?
 end
 end
