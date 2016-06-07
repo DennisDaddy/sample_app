31c653ab2affb1072e96697a1a32ff5b17259006
DennisDaddy <softteez95@gmail.com>
2016-06-07 10:31:01 +0300

    finish log in/log out

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -50,4 +50,9 @@ duplicate_user.email = @user.email.upcase
 @user.save
 assert_not duplicate_user.valid?
 end
+
+test "authenticated? should return false for a user with nil digest" do
+assert_not @user.authenticated?('')
+
+end
 end
