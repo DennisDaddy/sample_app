12775c508d0ab8d8cf067d36fb9dcffb10883803
DennisDaddy <softteez95@gmail.com>
2016-06-09 23:59:03 +0300

    show microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,6 +5,14 @@ class UserTest < ActiveSupport::TestCase
 @user = User.new(name: "Example User", email: "user@example.com" ,password: "foobar", password_confirmation: "foobar")
 end
 
+test "associated microposts should be destroyed" do
+	@user.save
+	@user.microposts.create!(content: "Lorem ipsum")
+	assert_difference 'Micropost.count', -1 do
+	@user.destroy
+end
+end
+
 test "password should have a minimum length" do
  @user.password = @user.password_confirmation = "a" * 5
  assert_not @user.valid?
